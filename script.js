748a1cbb338c4573c804605ccadad155d73ba5f2
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-05-01 11:30:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // your code here
+// your code here
+
+
+let name = document.getElementById("name").value;
+let year = document.getElementById("year").value;
+
+let button = document.getElementById("button");
+
+button.addEventListener("click",(e)=>btnfun(e));
+
+function btnfun(e){
+	e.preventDefault();
+name = document.getElementById("name").value;
+ year = document.getElementById("year").value;
+        let queryString = 'https://localhost:8080/';
+  let h3 = document.getElementById("url");
+	if(name!=""){
+		queryString += "?name="+name;
+		if(year!=""){
+			queryString +=  "&?year="+year;
+		}
+	}else if(year != null){
+			queryString +=  "?year="+year;
+	}
+ 
+h3.innerText = queryString;
+
+  // document.querySelector("#url").append(h3);
+}
